66944779074dbf227a8a8b61e93895d147b4c158
joemccann <joseph.isaac@gmail.com>
2020-07-17 15:35:09 -0700

    wip - flood requests

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,15 @@
 #! /usr/local/bin/node
 const fs = require('fs')
 const path = require('path')
-const { login, quote } = require('../src')
+const fetch = require('node-fetch')
 const args = process.argv.slice(2)
 const symbols = []
+
+const sleep = (ms = 1000) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
 
 const buildPairsFile = async () => {
   const file = await fs
@@ -28,38 +34,89 @@ const fetchInstrumentForSymbol = async () => {
   const file = await fs
     .readFileSync(path.resolve(__dirname, './pairs.json'), 'utf-8')
 
-  let token = null
-  let instrument = null
+  const pairs = JSON.parse(file)
+
+  const proms = []
+
+  const send = async (pair) => {
+    if (i % 5) await sleep(3000)
+    if (i > 50) return { data: null }
+    const { symbol } = pair
+    const URL = 'https://rh-api-instrument-from-symbol.azurewebsites.net/api/' +
+    `instrument?symbol=${symbol}`
+
+    let json = null
+    let resp = null
+    try {
+      console.log(`>>> Fetching ${symbol}`)
+      resp = await fetch(URL)
+      if (!resp) {
+        console.log('$$$$ NO RESPONSE $$$$')
+        console.log(URL)
+        return {
+          err: new Error('WTF')
+        }
+      }
+
+      const { status = 999 } = resp
+
+      if (status > 399) {
+        console.log('>>>>>>>>>>')
+        console.dir(resp.status)
+        return {
+          err: new Error(`Response not ok: ${resp.statusText} | ${resp.status}`)
+        }
+      }
+
+      json = await resp.json()
+    } catch (e) {
+      if (!resp) {
+        console.log('$$$$ NO RESPONSE EXCEPTION $$$$')
+        console.log(URL)
+        return {
+          err: new Error('WTF')
+        }
+      }
+      if (resp.status === 404) return { statusCode: 404 }
+      console.error(e.message)
+      return { err: e }
+    }
+    return { data: { instrument: json.body, symbol } }
+  }
+
+  let i = 0
+  for (const pair of pairs) {
+    proms.push(send(pair), i++)
+  }
+
+  const result = []
 
   try {
-    const { data } = await login()
-    token = data.access_token
-  } catch (e) {
-    console.error(e)
+    //
+    // Flatten the results as one array
+    //
+    (await Promise.all(proms)).map(r => {
+      //
+      // In the case r is undefined or null
+      //
+      if (r) {
+        const { data = null } = r
+        if (data) {
+          console.dir(data)
+          result.push(data)
+        }
+      }
+    })
+  } catch (err) {
+    console.error(err)
+    return { err }
   }
 
-  const pairs = JSON.parse(file)
-  const combined = {}
-
-  for (const pair of pairs) {
-    const { symbol: ticker } = pair
-    const params = {
-      token,
-      ticker
-    }
-
-    try {
-      const { data } = await quote(params)
-      instrument = data.instrument.split('/')[4]
-      combined[ticker] = instrument
-    } catch (e) {
-      console.error(e)
-    }
-  }
+  /// ///////////////////
 
   await fs
     .writeFileSync(path
-      .resolve(__dirname, './combined.json'), JSON.stringify(combined), 'utf-8')
+      .resolve(__dirname, './result.json'), JSON.stringify(result), 'utf-8')
 }
 
 const run = async () => {
@@ -67,7 +124,11 @@ const run = async () => {
     await buildPairsFile()
     console.info('✓ Built pairs.json file.')
   }
-  // await fetchInstrumentForSymbol()
+  try {
+    await fetchInstrumentForSymbol()
+  } catch (e) {
+    console.error(e)
+  }
   process.exit(0)
 }
 
